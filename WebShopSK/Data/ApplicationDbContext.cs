4dcf9f21353715b6b2ec38eb68ea37751a22bd9d
Silvija Klisovic <silvija.klisovic@sdworx.com>
2024-12-16 20:08:47 +0100

    Initaila commit

---
+++ WebShopSK/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShopSK.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(150)]
+        public string Adress { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WebShopSK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShopSK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
